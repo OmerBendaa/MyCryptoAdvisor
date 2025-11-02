4e5e6f3208c83a230af19ef6a5067b1de45f28c6
omerbenda <omerbd29@cs.colman.ac.il>
2025-11-02 21:15:06 +0200

    server is running

---
+++ server/src/server.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// mongoose.connect(process.env.MONGO_URI)
+// .then(() => console.log("MongoDB connected"))
+// .catch((err) => console.error("MongoDB connection error:", err));
+
+app.get("/", (req: express.Request, res: express.Response) => {
+    res.send("MyCryptoAdvisor is running");
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
