e8b79f812a6c67e68da0134b820d64fb7fd27424
omerbenda <omerbd29@cs.colman.ac.il>
2025-11-06 03:12:49 +0200

    :

--- client/src/common/constants.ts
+++ client/src/common/constants.ts
@@ -19,5 +19,5 @@ export const CONTENT_OPTIONS: string[] = [
   "Fun",
   "Education",
 ];
- export const API_USER_BASE = `https://mycryptoadvisor-production.up.railway.app/users`;
+ export const API_USER_BASE = `https://mycryptoadvisor-production.up.railway.app/users/`;
  
